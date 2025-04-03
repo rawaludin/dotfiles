8c70a3c7a303e828d5c7c7989a9a3b2625cbfdf7
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2025-04-03 12:46:32 +0700

    wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,18 +2,29 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.color_scheme = 'tokyonight_night'
 config.enable_tab_bar = false -- Using tmux instead
-config.font = wezterm.font { family = 'VictorMono Nerd Font Mono', weight = 'Bold' }
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font { family = 'VictorMono Nerd Font', weight = 'Bold' }
+-- config.font = wezterm.font { family = 'DroidSansM Nerd Font' }
+config.cursor_blink_rate = 0
 -- TODO: make font size 15 on mac air, 18 on mac mini
 config.font_size = 15
 config.line_height = 1.2
--- Slightly transparent and blurred background
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 30
-config.window_decorations = 'RESIZE'
-config.window_frame = {
-  font = wezterm.font { family = 'VictorMono Nerd Font Mono', weight = 'Bold' },
-  font_size = 12,
+config.mouse_bindings = {
+  -- Open a link
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = 'OpenLinkAtMouseCursor',
+  },
 }
+config.window_close_confirmation = 'NeverPrompt'
+config.window_decorations = 'RESIZE'
+-- config.window_padding = {
+--   left = 5,
+--   right = 5,
+--   top = 0,
+--   bottom = 0,
+-- }
 
 -- Set custom padding (top, right, bottom, left)
 config.window_padding = {
